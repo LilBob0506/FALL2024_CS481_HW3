db5ac319ec5beefb454a8b4d3396f666eaf5419a
LilBob0506 <131300719+LilBob0506@users.noreply.github.com>
2024-10-17 13:49:57 -0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,4 +2,4 @@
 cd FALL2024_CS481_HW3
 module load intel
 icx -O -o -fopenmp gameoflife_threaded1.c gameoflife1.c
-./gameoflife <size> <max number of generations> <number of threads> <output file directory>
+./gameoflife <size> <max number of generations> <number of threads> /scratch/ualclsd0193
