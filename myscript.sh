3c38c5bcee758084dc3e0efc19aab510dfe32835
LilBob0506 <131300719+LilBob0506@users.noreply.github.com>
2024-10-17 10:52:23 -0500

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd FALL2024_CS481_HW3
+module load intel
+icx -O -o -fopenmp gameoflife_threaded1.c gameoflife1.c
+./gameoflife <size> <max number of generations> <number of threads> <output file directory>
